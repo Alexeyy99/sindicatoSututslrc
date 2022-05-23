eb04cc7bd853b3bf0bf01674e6272b9e076fa542
Miguel A Padilla <70824040+mikealbert27@users.noreply.github.com>
2022-05-23 13:57:01 -0700

    Primera parte del proyecto
    Login, mision, vision, valores y index

---
+++ app/Http/Livewire/IniciarSesion/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire\IniciarSesion;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Login extends Component
+{
+
+    public $email;
+    public $password;
+
+    public function render()
+    {
+        return view('livewire.iniciar-sesion.login');
+    }
+
+    public function login(){
+        $datos = $this->validate();
+        
+        if (Auth::attempt($datos)){
+            return redirect(route('index'));
+        }else{
+            $this->emit('alerta', 'Verifica el correo o la contraseña');
+            $this->email = null;
+            $this->password = null;
+        }
+    }
+}
